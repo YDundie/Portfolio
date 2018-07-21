36e0cd1dec98099d168f4a060b512ec3e32a40e4
YDundie <mustacmarko@yahoo.com>
2018-07-21 11:57:57 +0200

    Adding some placeholder text

--- script.js
+++ script.js
@@ -2,41 +2,50 @@ var home = document.getElementById("home");
 var about = document.getElementById("about");
 var contact = document.getElementById("contact");
 var title = document.getElementById("title");
+var homecontent = document.getElementById("homecontent");
+var aboutcontent = document.getElementById("aboutcontent");
+var contactcontent = document.getElementById("contactcontent");
 
- 
+ hideContents();
+homecontent.style.display = "block";
 
 home.addEventListener("click",function(){
     
+    addFade();
+    hideContents();
     removeClass();
     home.classList.add("active");
- 
-    
+    homecontent.style.display = "block";
+    homecontent.classList.remove("fade");
     
 })
 
 title.addEventListener("click",function(){
-    
+    addFade();
+     hideContents();
     removeClass();
     home.classList.add("active");
- 
-    
+    homecontent.style.display = "block";
+    homecontent.classList.remove("fade");
     
 })
 
 about.addEventListener("click",function(){
-    
+     addFade();
+     hideContents();
     removeClass();
     about.classList.add("active");
- 
-    
+    aboutcontent.style.display = "block";
+    aboutcontent.classList.remove("fade");
     
 })
 contact.addEventListener("click",function(){
-    
+     addFade();
+     hideContents();
     removeClass();
     contact.classList.add("active");
- 
-    
+    contactcontent.style.display = "block";
+    contactcontent.classList.remove("fade");
     
 })
 
@@ -50,5 +59,21 @@ function removeClass(){
     about.classList.remove("active");
     contact.classList.remove("active");
     title.classList.remove("active");
+    
+}
+
+function hideContents(){
+    
+    contactcontent.style.display = "inline";
+    aboutcontent.style.display = "inline";
+    homecontent.style.display = "inline";
+    
+}
+
+function addFade(){
+    
+      contactcontent.classList.add("fade")
+    aboutcontent.classList.add("fade")
+    homecontent.classList.add("fade")
     
 }
